3d7be67acb60b397d2bc1e7894e3d4a655e6a594
Silje32 <msh070425@gmail.com>
2025-08-06 10:44:55 +0200

    Added try catch

--- src/modules/fetchPokemon.js
+++ src/modules/fetchPokemon.js
@@ -1,7 +1,11 @@
 import { pokemonApiUrl } from "../config.js";
 
 export const fetchPokemon = async (pokemon) => {
-  await fetch(`${pokemonApiUrl}/pokemon/${pokemon}`);
-  const pokemonData = await Response.json();
+  const response = await fetch(`${pokemonApiUrl}/pokemon/${pokemon}`);
+  try {
+    const pokemonData = await response.json();
+  } catch (err) {
+    throw new Error("Failed to get JSON from fetchPokemon");
+  }
   return pokemonData;
 };
