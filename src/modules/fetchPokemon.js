8bad706be1ed97bc525be2b6ebbb686aa0166bcf
Silje32 <msh070425@gmail.com>
2025-08-06 10:51:02 +0200

    Added if statement

--- src/modules/fetchPokemon.js
+++ src/modules/fetchPokemon.js
@@ -2,6 +2,11 @@ import { pokemonApiUrl } from "../config.js";
 
 export const fetchPokemon = async (pokemon) => {
   const response = await fetch(`${pokemonApiUrl}/pokemon/${pokemon}`);
+
+  if (response.status !== 304 || response.status !== 200) {
+    throw new Error("Failed to fetch pokemon");
+  }
+
   try {
     const pokemonData = await response.json();
   } catch (err) {
