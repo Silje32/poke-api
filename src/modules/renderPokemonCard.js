ecb7b1f9fb54e37241e5efb9378ae5bc4322674e
Silje32 <msh070425@gmail.com>
2025-08-07 12:13:52 +0200

    Updated my renderPokemonCard function

--- src/modules/renderPokemonCard.js
+++ src/modules/renderPokemonCard.js
@@ -1,20 +1,49 @@
+import { renderPokemonImage } from "./renderPokemonImage";
+
 // Bilde av pokemon, knapper, navn
 export const renderPokemonCard = (pokemonCardElement, pokemonObj) => {
   // Sette inn et bilde
   const imgElement = document.createElement("img");
   imgElement.src = pokemonObj.sprites.front_default;
+
   // Sette inn tekst for navn!
   const nameElement = document.createElement("h1");
   nameElement.textContent = pokemonObj.name;
 
-  // Append elementer
-  pokemonCardElement.replaceChildren(imgElement, nameElement);
+  let sex = "male";
+  let viewDirection = "front";
+  let shiny = false;
 
-  /* Alt. Data...
-  - Abilities?
-  - Types?
-  */
+  // Rotate Button
+  const rotateBtn = document.createElement("button");
+  rotateBtn.textContent = "Rotate";
+  rotateBtn.addEventListener("click", () => {
+    (viewDirection === viewDirection) === "front" ? "back" : "front";
+    imgElement.src = renderPokemonImage(pokemonObj, shiny, sex, viewDirection);
+  });
 
-  // Tømme forrige innholdet
-  pokemonCardElement.replaceChildren();
+  // Shiny Button
+  const shinyBtn = document.createElement("button");
+  shinyBtn.textContent = "Toggle Shiny";
+  shinyBtn.addEventListener("click", () => {
+    shiny = !shiny;
+    imgElement.src = renderPokemonImage(pokemonObj, shiny, sex, viewDirection);
+  });
+
+  // Toggle Female
+  const swapGenderBtn = document.createElement("button");
+  swapGenderBtn.textContent = "Swap Gender";
+  swapGenderBtn.addEventListener("click", () => {
+    sex = sex === "male" ? "female" : "male";
+    imgElement.src = renderPokemonImage(pokemonObj, shiny, sex, viewDirection);
+  });
+
+  // Append elements
+  pokemonCardElement.replaceChildren(
+    imgElement,
+    nameElement,
+    rotateBtn,
+    shinyBtn,
+    swapGenderBtn
+  );
 };
