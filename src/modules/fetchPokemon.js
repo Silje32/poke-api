277b9148c236b93a37630b572e232e168271100d
Silje32 <msh070425@gmail.com>
2025-08-07 10:22:06 +0200

    Fixed a bug

--- src/modules/fetchPokemon.js
+++ src/modules/fetchPokemon.js
@@ -2,11 +2,11 @@ import { pokemonApiUrl } from "../config.js";
 
 export const fetchPokemon = async (pokemon) => {
   const response = await fetch(`${pokemonApiUrl}/pokemon/${pokemon}`);
-
   if (response.status !== 304 && response.status !== 200) {
-    throw new Error("Failed to fetch pokemon");
+    throw new Error("Failed to fetch pokemon.");
   }
 
+  let pokemonData = null;
   try {
     const pokemonData = await response.json();
   } catch (err) {
