7cba3d1210a44f65b2330a14c767efa32db29307
Silje32 <msh070425@gmail.com>
2025-08-07 12:52:12 +0200

    Fixed a few bugs

--- src/modules/renderPokemonImage.js
+++ src/modules/renderPokemonImage.js
@@ -1,25 +1,30 @@
 const getFemaleImage = (pokemonObj, shiny, direction) => {
-    return shiny ? (
-        direction === "front" ? pokemonObj.sprites.front_shiny_female : pokemonObj.sprites.back_shiny_female
-    ) : (
-        direction === "front" ? pokemonObj.sprites.front_female : pokemonObj.sprites.back_shiny_female
-    )
+  return shiny
+    ? direction === "front"
+      ? pokemonObj.sprites.front_shiny_female
+      : pokemonObj.sprites.back_shiny_female
+    : direction === "front"
+    ? pokemonObj.sprites.front_female
+    : pokemonObj.sprites.back_female;
 };
 
 const getMaleImage = (pokemonObj, shiny, direction) => {
-  return shiny ? (direction === "front" ? pokemonObj.sprites.front_shiny : pokemonObj.sprites.back_shiny
-  ) : (
-    direction === "front" ? pokemonObj.sprites.front_shiny : pokemonObj.sprites.back_default;
-  )
+  return shiny
+    ? direction === "front"
+      ? pokemonObj.sprites.front_shiny
+      : pokemonObj.sprites.back_shiny
+    : direction === "front"
+    ? pokemonObj.sprites.front_default
+    : pokemonObj.sprites.back_default;
 };
 
 export const renderPokemonImage = (pokemonObj, shiny, sex, direction) => {
-    let imageSource = undefined;
+  let imageSource = undefined;
 
-    if (sex === "male") {
-        imageSource = getMaleImage(pokemonObj, shiny, direction)
-   } else {
-        imageSource = getFemaleImage(pokemonObj, shiny, direction)
-   }
-   return imageSource;
+  if (sex === "male") {
+    imageSource = getMaleImage(pokemonObj, shiny, direction);
+  } else {
+    imageSource = getFemaleImage(pokemonObj, shiny, direction);
+  }
+  return imageSource;
 };
